0e3c3cee9ac588c868067b579edbff786f87cd53
Nalsolniel <thlapegue88@gmail.com>
2020-05-23 19:42:46 +0200

    point

--- exo9_9/src/main/java/fr/uvsq/exo9_9/Point.java
+++ exo9_9/src/main/java/fr/uvsq/exo9_9/Point.java
@@ -1,5 +1,32 @@
 package fr.uvsq.exo9_9;
 
 public class Point {
-
+	private int x;
+	private int y;
+	
+	
+	  public Point(final int X, final int Y) {
+	        this.x = X;
+	        this.y = Y;
+	    }
+     public Point () {
+    	 this.x = 0;
+	     this.y = 0;
+     }
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public String Aff() {
+        return "(" + x + "," + y + ")";
+    }
 }
